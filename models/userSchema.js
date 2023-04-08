1a8d9b17cf15946b190101f11baed73c5b62de0a
Botirjon Shokirov <botirjon@radcode.co>
2023-04-08 06:32:41 +0200

    added jwt token

--- models/userSchema.js
+++ models/userSchema.js
@@ -3,9 +3,27 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 10000, // Default balance of 10,000
+  },
+  fakeMoney: {
+    type: Number,
+    default: 10000, // Default fake money of 10,000
+  },
 });
 
 export default userSchema;
